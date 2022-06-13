c496961b4ba20cde217c3fa3ff6d365216e1a7f2
Ada George <adageorge79@gmail.com>
2022-06-13 00:22:53 -0500

    adding the url

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import {FaGithub} from 'react-icons/fa'
 const Footer = () => {
   return (
     <footer className='footer'>
-       <a href="#" target='_blank'><FaGithub/></a> 
+       <a href="https://github.com/AdaGeorge/Rick-and-Morty-Location-Finder" target='_blank'><FaGithub/></a> 
     </footer>
   )
 }
