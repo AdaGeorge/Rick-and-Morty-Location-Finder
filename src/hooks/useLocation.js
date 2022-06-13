66e7dfe1e6e3acdbf5ccfa905cf9ec76dd43f071
Ada George <adageorge79@gmail.com>
2022-06-13 12:03:00 -0500

    import axios

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -1,5 +1,5 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 
 const useLocation = (searchedValue) => {
